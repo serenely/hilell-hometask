343902c25b762287997e044344f10227511b6388
serenely <70910099+serenely@users.noreply.github.com>
2023-02-21 19:39:39 +0200

    Update eventListener.js

--- myBad/eventListener.js
+++ myBad/eventListener.js
@@ -1,12 +1,5 @@
-// На сторінці є дві кнопки. - переадресовується на інший сайт (за раніше введеним посиланням).
-// Реалізувати перевірку на http / https. Якщо протокол не вказаний - додаємо
-
-
 const aPerevirkaFirst = document.querySelector('.first__link')
 const aPerevirkaSecond = document.querySelector('.second__link')
-
-console.log(aPerevirkaFirst.href);
-console.log(aPerevirkaSecond);
 
 if (aPerevirkaFirst.href.includes('http') || aPerevirkaFirst.href.includes('https')) {
     console.log(true);
